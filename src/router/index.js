b47b49880ec88630b249b21bafb86ff1a11c4d95
GordonOtieno <otienogordon95@gmail.com>
2023-04-23 13:23:54 +0300

    Update reuter files

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,8 @@ import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import RegisterView from '../views/RegisterView.vue'
 import LoginView from '../views/LoginView.vue'
+import AccountView from '../views/AccountView.vue'
+import ProfileSection from '../views/account/ProfileSection.vue'
 
 const routes = [
   {
@@ -20,6 +22,18 @@ const routes = [
         path: '/login',
         name: 'home',
         component:LoginView
+    
+      },
+      {
+        path: '/account',
+        component:AccountView,
+        children:[
+          {
+            path: 'profile',
+            name: 'profileSection',
+            component:ProfileSection
+          }
+        ]
     
       },
  
