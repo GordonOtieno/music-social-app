3d08c23663cd69ea3f2f9bcc31adbc20809681ee
GordonOtieno <otienogordon95@gmail.com>
2023-04-25 22:39:37 +0300

    Update AccountView.vue component

--- src/views/AccountView.vue
+++ src/views/AccountView.vue
@@ -1,10 +1,13 @@
 <template>
-    <div id="AcountView" class="max-w-4xl mx-auto">
+    <TopNavigation />
+    <div id="AcountView" class="max-w-4xl mx-auto mt-10">
         <router-view/>
     </div>
+    <FooterSection />
   </template>
   <script setup>
-
+  import TopNavigation from '@/components/stracture/TopNavigation.vue';
+  import FooterSection from '@/components/stracture/FooterSection.vue';
   </script>
   
   <style lang="scss">
