c989ac8358f7263933420afa8fb4e088f3b8fcdf
GordonOtieno <otienogordon95@gmail.com>
2023-04-27 21:47:02 +0300

    Create and update DeleteYouTubeVideo component

---
+++ src/views/account/DeleteYoutubeVideo.vue
@@ -0,0 +1,38 @@
+<template>
+    <div id="DeleteProfileContainer" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
+        <div class="text-gray-900 text-xl">Delete Song</div>
+        <div class="bg-red-500 w-full h-1 mb-4"></div> 
+
+        <div class="bg-white rounded px-8 pt-6 pb-8">
+            <div class="flex flex-wrap justify-between">
+                <div class="w-1/4 mr-auto mt-2 text-lg p-1 text-gray-900">
+                    1. My first youtube video
+                    <iframe class="w-full h-20" src="https://www.youtube.com/embed/RhwRztNLAo8" frameborder="0"></iframe>
+                </div>
+                <button class="float-right
+                               bg-transparent
+                               hover:bg-red-500
+                               text-gray-900
+                               font-semibold
+                               hover:text-white
+                               py-2
+                               px-4
+                               border
+                               border-red-500
+                               hover:border-transparent
+                               rounded
+                       ">Delete</button>
+            </div>
+        </div>
+               
+           
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
