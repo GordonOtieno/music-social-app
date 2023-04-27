517b8a1d41fda58f51d881ec52346dd98c63888f
GordonOtieno <otienogordon95@gmail.com>
2023-04-27 21:44:54 +0300

    Create and update AddYoutubeVodeo component

---
+++ src/views/account/AddYoutubeVideo.vue
@@ -0,0 +1,45 @@
+<template>
+    <div id="editProfileContainer" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
+        <div class="text-gray-900 text-xl">Add Song</div>
+        <div class="bg-green-500 w-full h-1 mb-4"></div> 
+
+
+                <TextInput 
+                    label="Title"
+                    placeholder="Cool New Music"
+                    v-model:input="link"
+                    inputType="text"
+                    error="This is a text error"
+                    />
+
+                    <TextInput 
+                    label="Link"
+                    placeholder="RhwRztNLAo8"
+                    v-model:input="link"
+                    inputType="text"
+                    error="This is a text error"
+                    />
+                    
+            <div class="flex flex-wrap mt-8 mb-6">
+            <div class="w-full px-3">
+                <SubmitFormBtn
+                    btnText="Add Video"
+               />
+            </div>
+        </div>
+              
+   
+        
+    </div>
+</template>
+
+<script setup>
+import TextInput from '@/components/global/TextInput.vue';
+import SubmitFormBtn from '@/components/global/SubmitFormBtn.vue';
+
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
