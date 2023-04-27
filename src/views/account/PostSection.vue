55dbc61ad0841fe600556e030300bdc4209365dd
GordonOtieno <otienogordon95@gmail.com>
2023-04-27 21:48:19 +0300

    Create and update PostSection component

---
+++ src/views/account/PostSection.vue
@@ -0,0 +1,34 @@
+<template>
+    <div id="PostsContainer" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
+        <div class="text-gray-900 text-xl">Post</div>
+        <div class="bg-green-500 w-full h-1"></div>
+        <div class="mx-auto">
+            <div class="my-4">
+                <div class="flex items-center py-2">
+                    <img src="https://via.placeholder.com/50" class="rounded-full" alt="" width="50">
+                    <div class="ml-2 font-bold text-2xl">Name Here</div>
+                </div>
+                <img src="https://via.placeholder.com/800" alt="">
+                <div class="p-4">
+                    <p class="text-3xl font-bold hover:text-gray-700 pb-4">This is a Title</p>
+                    <p class="py-2 text-lg">Event Location: Nairobi kenya</p>
+                    <p class="pb-6">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
+                        Magnam vero, ipsum obcaecati dolor officia soluta eveniet at reiciendis
+                         perspiciatis dignissimos voluptates pariatur aliquam molestias culpa 
+                        tenetur harum dolores ad? Eius.
+                    </p>
+                </div>
+            </div>
+        </div>
+       
+        </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="sass" scoped>
+
+</style>
