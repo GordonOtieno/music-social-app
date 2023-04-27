5899ef4d888e9d19168e93da0755c65a4661e77b
GordonOtieno <otienogordon95@gmail.com>
2023-04-27 21:41:19 +0300

    Update router file add links

--- src/router/index.js
+++ src/router/index.js
@@ -5,6 +5,15 @@ import LoginView from '../views/LoginView.vue'
 import AccountView from '../views/AccountView.vue'
 import ProfileSection from '../views/account/ProfileSection.vue'
 import EditProfile from '../views/account/EditProfile.vue'
+import AddSong from '../views/account/AddSong.vue'
+import DeleteSong from '../views/account/DeleteSong.vue'
+import AddYoutubeVideo from '../views/account/AddYoutubeVideo.vue'
+import DeleteYoutubeVideo from '../views/account/DeleteYoutubeVideo.vue'
+import CreatePost from '../views/account/CreatePost.vue'
+import EditPost from '../views/account/EditPost.vue'
+import Posts from '../views/account/PostsSection.vue'
+import Post from '../views/account/PostSection.vue'
+
 
 const routes = [
   {
@@ -38,6 +47,46 @@ const routes = [
             path: 'edit-profile',
             name: 'EditProfile',
             component: EditProfile,
+          },
+          {
+            path: 'add-song',
+            name: 'AddSong',
+            component: AddSong,
+          },
+          {
+            path: 'delete-song',
+            name: 'DeleteSong',
+            component: DeleteSong,
+          },
+          {
+            path: 'add-youtube-video',
+            name: 'AddYoutubeVideo',
+            component: AddYoutubeVideo,
+          },
+          {
+            path: 'delete-youtube-video',
+            name: 'DeleteYoutubeVideo',
+            component: DeleteYoutubeVideo,
+          },
+          {
+            path: 'create-post',
+            name: 'CreatePost',
+            component: CreatePost,
+          },
+          {
+            path: 'edit-post',
+            name: 'EditPost',
+            component: EditPost,
+          },
+          {
+            path: 'posts',
+            name: 'Posts',
+            component: Posts,
+          },
+          {
+            path: 'post',
+            name: 'Post',
+            component: Post,
           },
         ]
     
