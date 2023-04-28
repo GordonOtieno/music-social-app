55b7a61851c2d779208ee4526f842464477b7843
GordonOtieno <otienogordon95@gmail.com>
2023-04-28 22:13:44 +0300

    Update TopNavigation Component

--- src/components/stracture/TopNavigation.vue
+++ src/components/stracture/TopNavigation.vue
@@ -3,7 +3,7 @@
         <div class="container mx-auto max-w-4xl h-16 flex justify-between text-xs md:text-sm">
             <div class="h-full flex items-center">
                 <router-link to="" class="flex justify-centre text-centre">
-                    <div class="text-white text-2xl"><b>MSN</b></div>
+                    <div class="text-white text-2xl"> <router-link to="/"><b>MSN</b></router-link></div>
                 </router-link>
             </div>
             <div class="h-full flex item-centre">
