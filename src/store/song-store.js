8f3711455d3f2a7faf0e03669d91536a47f5ab07
GordonOtieno <otienogordon95@gmail.com>
2023-05-29 20:58:36 +0300

    Create src/store/song-store.js file

---
+++ src/store/song-store.js
@@ -0,0 +1,25 @@
+
+import axios from 'axios'
+import { defineStore } from 'pinia'
+
+export const useSongStore = defineStore('song', {
+  state: () => ({
+    artistId: null,
+    artistName: null,
+    songs: null,
+  }),
+
+  actions: {
+        async fetchSongByUserId(userId){
+        let res = await axios.get('user/' + userId + '/songs')
+        this.$state.artistId = res.data.artist_id
+        this.$state.artistName = res.data.artist_name
+        this.$state.songs = res.data.songs
+    },
+
+    async clearSong(){
+        this.$state.songs = null
+    },
+  },
+  persist: true,
+})
