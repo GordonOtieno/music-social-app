28b19ea03da7cf8c8629a3f7c267f28b0ac7a78b
GordonOtieno <otienogordon95@gmail.com>
2023-06-01 23:33:06 +0300

    Update CreatePost.vue component

--- src/views/account/CreatePost.vue
+++ src/views/account/CreatePost.vue
@@ -18,7 +18,7 @@
                     placeholder="Awesome Concert"
                     v-model:input="title"
                     inputType="text"
-                    error="This is a text error"
+                    :error="errors.title ? errors.title[0] : ''"
                     />
                 </div>
                     
@@ -28,7 +28,7 @@
                     placeholder="Nairobi"
                     v-model:input="location"
                     inputType="text"
-                    error="This is a text error"
+                    :error="errors.location ? errors.location[0] : ''"
                 />
             </div> 
         </div>       
@@ -58,8 +58,8 @@
                 <TextAreaView
                     label="Description"
                     placeholder="Please enter some information here"
-                    v-model="description"
-                    error="This is a test error"
+                    v-model:description="description"
+                    :error="errors.description ? errors.description[0] : ''"
                 />
             </div>
         </div>
@@ -67,6 +67,7 @@
             <div class="w-full px-3">
                 <SubmitFormBtn
                     btnText="Create Post"
+                    @submit="createPost"
                />
             </div>
         </div>
@@ -74,24 +75,68 @@
 
 <script setup>
 import { ref } from 'vue';
-
+import axios from 'axios';
+import Swal from '@/sweetalert2';
 import TextInput from '@/components/global/TextInput.vue';
 import DisplayCropperButton from '../../components/global/DisplayCropperButton.vue'
 import TextAreaView from '../../components/global/TextAreaView.vue'
 import SubmitFormBtn from '../../components/global/SubmitFormBtn.vue'
 import CropperModal from '../../components/global/CropperModal.vue'
 import CroppedImage from '../../components/global/CroppedImage.vue'
+import { useUserStore } from '@/store/user-store';
+import { useRouter } from 'vue-router';
+
+const userStore = useUserStore();
+const router = useRouter()
 
 let title = ref(null)
 let showModal = ref(false)
 let location = ref(null)
 let description = ref(null)
-// let imageData = null
+let imageData = null
 let image = ref(null)
+let errors = ref([]);
+
 const setCroppedImageData = (data) => {
-    // imageData= data
+    imageData= data
     image.value = data.imageUrl
 }
+
+const createPost = async () =>{
+    errors.value=[];
+    let data = new FormData();
+
+  
+    data.append('user_id', userStore.id || '')
+    data.append('title', title.value || '')
+    data.append('location', location.value || '')
+    data.append('description', description.value || '')
+    
+    if(imageData){
+        data.append('image', imageData.file || '')
+    data.append('height', imageData.height || '')
+    data.append('width', imageData.width || '')
+    data.append('left', imageData.left || '')
+    data.append('top', imageData.top || '')
+    }
+
+
+    try{
+        await axios.post('posts/',data)
+
+        Swal.fire(
+                'Post Created!',
+                'You\'ve Successfully created '+ title.value +' post.',
+                'success'
+                )
+
+        router.push('/account/profile')
+
+    }catch(err){
+        errors.value = err.response.data.errors;
+    }
+}
+
 </script>
 
 <style lang="sass" scoped>
