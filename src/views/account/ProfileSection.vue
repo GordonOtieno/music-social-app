84820b121458c94dcc707d99e2aeb6840ee6acb2
GordonOtieno <otienogordon95@gmail.com>
2023-06-03 21:28:56 +0300

    Update ProfileSection.vue component file

--- src/views/account/ProfileSection.vue
+++ src/views/account/ProfileSection.vue
@@ -1,21 +1,21 @@
 <template>
     <div class="container max-w-4xl mx-auto flex py-5">
        <div class="w-1/3">
-        <img :src="userStore.image" alt="profile pic" 
+        <img :src="profileStore.image" alt="profile pic" 
         class="w-full rounded-lg h-auto shadow-lg" />
        </div>
        <div class="w-full pl-4">
         <div class="flex">
             <div class="w-1/2">
                 <h1 class="text-2xl md:text-4xl text-left text-gray-900">
-                    {{ userStore.firstName }} {{ userStore.lastName }}
+                    {{ profileStore.firstName }} {{ profileStore.lastName }}
                 </h1>
                 <span class="text-md text-gray-700">
-                    <i><b>{{ userStore.location }}</b></i>
+                    <i><b>{{ profileStore.location }}</b></i>
                 </span>
             </div>
 
-            <div class="w-1/2">
+            <div  v-if="userStore.id == route.params.id" class="w-1/2">
                 <RouterLinkButton
                     btnText="Edit Profile"
                     color="green"
@@ -41,8 +41,17 @@ import YouTubeSection from '@/components/partials/profile/YouTubeSection.vue';
 import PostsSection from '../../components/partials/profile/PostsSection.vue';
 
 import { useUserStore } from '../../store/user-store'
+import { useProfileStore } from '../../store/profile-store'
+import { useRoute } from 'vue-router';
+import { onMounted } from 'vue';
 
 const userStore = useUserStore()
+const profileStore = useProfileStore();
+const route = useRoute();
+
+onMounted(async()=>{
+    await profileStore.fetchProfileById(route.params.id)
+})
 
 </script>
 
