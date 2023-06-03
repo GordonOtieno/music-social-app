02ac3b1605117f232ba48dd0782c91d3164bc7ab
GordonOtieno <otienogordon95@gmail.com>
2023-06-03 21:37:36 +0300

    Update EditPost.vue component file

--- src/views/account/EditPost.vue
+++ src/views/account/EditPost.vue
@@ -18,7 +18,7 @@
                     placeholder="Awesome Concert"
                     v-model:input="title"
                     inputType="text"
-                    error="This is a text error"
+                    :error="errors.title ? errors.title[0] : ''"
                     />
                 </div>
                     
@@ -28,7 +28,7 @@
                     placeholder="Nairobi"
                     v-model:input="location"
                     inputType="text"
-                    error="This is a text error"
+                    :error="errors.location ? errors.location[0] : ''"
                 />
             </div> 
         </div>       
@@ -58,8 +58,8 @@
                 <TextAreaView
                     label="Description"
                     placeholder="Please enter some information here"
-                    v-model="description"
-                    error="This is a test error"
+                    v-model:description="description"
+                    e :error="errors.description ? errors.description[0] : ''"
                 />
             </div>
         </div>
@@ -67,31 +67,100 @@
             <div class="w-full px-3">
                 <SubmitFormBtn
                     btnText="Update Post"
+                    @submit="updatePost"
                />
             </div>
         </div>
 </template>
 
 <script setup>
-import { ref } from 'vue';
-
+import { onMounted, ref } from 'vue';
+import axios from 'axios';
+import { useRoute,useRouter } from 'vue-router';
 import TextInput from '@/components/global/TextInput.vue';
 import DisplayCropperButton from '../../components/global/DisplayCropperButton.vue'
 import TextAreaView from '../../components/global/TextAreaView.vue'
 import SubmitFormBtn from '../../components/global/SubmitFormBtn.vue'
 import CropperModal from '../../components/global/CropperModal.vue'
 import CroppedImage from '../../components/global/CroppedImage.vue'
+import Swal from '@/sweetalert2';
+
+import { usePostStore } from '@/store/post-store';
+import { useUserStore } from '@/store/user-store';
+
+
+const route = useRoute();
+const router = useRouter();
+const postStore = usePostStore();
+const userStore = useUserStore();
 
 let title = ref(null)
 let showModal = ref(false)
 let location = ref(null)
 let description = ref(null)
-// let imageData = null
+let errors = ref([]);
+let imageData = null
 let image = ref(null)
+
+onMounted(async () => {
+    await getPostById();
+ })
+
+
 const setCroppedImageData = (data) => {
-    // imageData= data
+    imageData= data
     image.value = data.imageUrl
 }
+
+const getPostById = async () => {
+    try{
+        let res = await axios.get('posts/'+ route.params.id);
+        title.value = res.data.title;
+        location.value = res.data.location;
+        image.value = postStore.postImage(res.data.image)
+        description.value = res.data.description;
+    }catch(err){
+       errors.value = err.response.data.errors;
+    }
+    
+}
+
+const updatePost = async () =>{
+    errors.value=[];
+
+    let data = new FormData();
+
+    data.append('title', title.value || '')
+    data.append('location', location.value || '')
+    data.append('description', description.value || '')
+    
+    if(imageData){
+    data.append('id', userStore.id || '')
+    data.append('image', imageData.file || '')
+    data.append('height', imageData.height || '')
+    data.append('width', imageData.width || '')
+    data.append('left', imageData.left || '')
+    data.append('top', imageData.top || '')
+    }
+
+
+    try{
+        await axios.post('posts/'+ route.params.id + '?_method=PUT', data)
+        await postStore.fetchPostsByUserId(userStore.id)
+
+        Swal.fire(
+                'Post Updated!',
+                'You\'ve Successfully Updated '+ title.value +'.',
+                'success'
+                )
+
+        router.push('/account/profile')
+
+    }catch(err){
+        errors.value = err.response.data.errors;
+    }
+}
+
 </script>
 
 <style lang="sass" scoped>
