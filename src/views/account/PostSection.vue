e80bac82d87b9b8820f548decc86f15a81670165
GordonOtieno <otienogordon95@gmail.com>
2023-06-03 21:38:18 +0300

    Update PostSection.vue component file

--- src/views/account/PostSection.vue
+++ src/views/account/PostSection.vue
@@ -1,31 +1,54 @@
 <template>
-    <div id="PostsContainer" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
-        <div class="text-gray-900 text-xl">Post</div>
+    <div v-if="post" id="PostsContainer" class="container max-w-4xl mx-auto pt-20 pb-20 px-6">
+        <div class="text-gray-900 text-xl">Post {{ post.id }}</div>
         <div class="bg-green-500 w-full h-1"></div>
         <div class="mx-auto">
             <div class="my-4">
                 <div class="flex items-center py-2">
-                    <img src="https://via.placeholder.com/50" class="rounded-full" alt="" width="50">
-                    <div class="ml-2 font-bold text-2xl">Name Here</div>
+                    <img :src="userStore.userImage(post.user.image)" class="rounded-full" alt="" width="50">
+                    <div class="ml-2 font-bold text-2xl">{{ post.user.first_name }} {{ post.user.last_name }}</div>
                 </div>
-                <img src="https://via.placeholder.com/800" alt="">
+                <img class="w-full" :src="postStore.postImage(post.image)" alt="">
                 <div class="p-4">
-                    <p class="text-3xl font-bold hover:text-gray-700 pb-4">This is a Title</p>
-                    <p class="py-2 text-lg">Event Location: Nairobi kenya</p>
+                    <p class="text-3xl font-bold hover:text-gray-700 pb-4">{{post.title}}</p>
+                    <p class="py-2 text-lg">Event Location: {{post.location}}</p>
                     <p class="pb-6">
-                        Lorem ipsum dolor sit amet consectetur adipisicing elit. 
-                        Magnam vero, ipsum obcaecati dolor officia soluta eveniet at reiciendis
-                         perspiciatis dignissimos voluptates pariatur aliquam molestias culpa 
-                        tenetur harum dolores ad? Eius.
+                        {{post.description}}
                     </p>
                 </div>
             </div>
-        </div>
+        </div>      
 
         </div>
 </template>
 
 <script setup>
+import { usePostStore } from "@/store/post-store";
+import { useUserStore } from "@/store/user-store";
+import axios from "axios";
+import { onMounted, ref } from "vue";
+import { useRoute } from 'vue-router'
+
+const postStore = usePostStore();
+const userStore = useUserStore();
+
+let post= ref(null);
+const route = useRoute();
+
+onMounted(async ()=>{
+   await getPostById();
+})
+
+const getPostById = async () => {
+   try{
+
+    let res = await axios.get('posts/' + route.params.id)
+    post.value = res.data;
+
+   }catch(err){
+    console.log(err);
+   }
+}
 
 </script>
 
